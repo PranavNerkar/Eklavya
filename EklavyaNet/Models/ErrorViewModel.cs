660d3d04d55a1d38df5787699ab744f15d4ed872
PranavNerkar <pranavnerkar321@gmail.com>
2024-03-07 14:04:22 +0530

    final updated project

--- EklavyaNet/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace EklavyaNet.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
